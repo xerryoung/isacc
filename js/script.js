71c50f0918afcad7ffb1c8ba3135175c7c627b08
zerry <shdud1012@gmail.com>
2025-12-09 12:42:41 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-
-// 데스크탑 
+// 상세메뉴 펼치기
 const items = document.querySelectorAll('.top_menu');
 items.forEach(li=>{
-    const i = li.querySelector('.sub_menu');
-    if (!i) return;
+  const i = li.querySelector('.sub_menu');
+  if (!i) return;
 
-    let timer = null;
-    const show = () => {
-        clearTimeout(timer); i.style.display = 'block'};
-    const hide = () => {
-        timer = setTimeout(()=>{i.style.display = 'none'}, 200);};
-    li.addEventListener('mouseover',show);
-    li.addEventListener('mouseleave',hide);
+  let timer = null;
+  const show = () => {
+    clearTimeout(timer); i.style.display = 'block'};
+  const hide = () => {
+    timer = setTimeout(()=>{i.style.display = 'none'}, 200);};
+  li.addEventListener('mouseover',show);
+  li.addEventListener('mouseleave',hide);
+});
+
+// 스와이퍼
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
